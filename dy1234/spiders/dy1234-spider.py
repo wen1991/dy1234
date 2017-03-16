0429ffc6d1b671049c4dc089bb88f92cf80fddb8
wen1991 <halfwill_being@sina.com>
2017-03-16 20:09:51 +0800

    学习rule

--- dy1234/spiders/dy1234-spider.py
+++ dy1234/spiders/dy1234-spider.py
@@ -5,17 +5,36 @@ from scrapy.contrib.linkextractors import LinkExtractor
 from dy1234.items import *
 
 
+# class DY1234Spider(CrawlSpider):
+
+#     name = 'dy1234'
+#     allowed_domains = ['idyjy.com']
+#     start_urls = ['http://www.idyjy.com']
+#     # start_urls = ['http://www.idyjy.com/sub/23182.html']
+#     rules = [Rule(LinkExtractor(allow=['/sub/\d+\.html']),
+#                   callback='parse_torrent')]
+
+#     def parse_torrent(self, response):
+#         tt = urlItem()
+#         tt['url'] = response.url
+#         tt['description'] = response.xpath(
+#             "//h1/span[@id='name']/text()").extract()
+#         yield tt
+#         for tt in response.xpath("//ul/li/input[@class='down_url']"):
+#             torrent = TorrentItem()
+#             torrent['title'] = tt.xpath('@file_name').extract()
+#             torrent['link'] = tt.xpath('@value').extract()
+#             # torrent['description'].append(description)
+#             # print '[+]%s' % (title)
+#             # print '[+]%s' % (link)
+#             yield torrent
+
+##########################################################################
 class DY1234Spider(CrawlSpider):
 
     name = 'dy1234'
     allowed_domains = ['idyjy.com']
-    # start_urls = ['http://www.idyjy.com']
     start_urls = ['http://www.idyjy.com/sub/23182.html']
-    # rules = [Rule(LinkExtractor(allow=['/sub/\d+\.html']), 'parse_torrent')]
-
-    # def parse_torrent(self, response):
-
-    #     return torrent
 
     def parse(self, response):
         tt = urlItem()
@@ -27,7 +46,6 @@ class DY1234Spider(CrawlSpider):
             torrent = TorrentItem()
             torrent['title'] = tt.xpath('@file_name').extract()
             torrent['link'] = tt.xpath('@value').extract()
-            # torrent['description'].append(description)
             # print '[+]%s' % (title)
             # print '[+]%s' % (link)
             yield torrent
