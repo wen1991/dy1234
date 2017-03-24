b466b63742eabbda775bb0aabc007c6dbade8d32
wen1991 <halfwill_being@sina.com>
2017-03-24 15:04:47 +0800

    delete unused code

--- dy1234/spiders/dy1234-spider.py
+++ dy1234/spiders/dy1234-spider.py
@@ -4,35 +4,6 @@ from scrapy.contrib.linkextractors import LinkExtractor
 
 from dy1234.items import *
 
-# import MySQLdb
-
-# class DY1234Spider(scrapy.Spider):
-#     name = 'author'
-
-#     start_urls = ['http://quotes.toscrape.com/']
-
-#     def parse(self, response):
-#         # follow links to author pages
-#         for href in response.css('.author + a::attr(href)').extract():
-#             yield scrapy.Request(response.urljoin(href),
-#                                  callback=self.parse_author)
-
-#         # follow pagination links
-#         next_page = response.css('li.next a::attr(href)').extract_first()
-#         if next_page is not None:
-#             next_page = response.urljoin(next_page)
-#             yield scrapy.Request(next_page, callback=self.parse)
-
-#     def parse_author(self, response):
-#         def extract_with_css(query):
-#             return response.css(query).extract_first().strip()
-
-#         yield {
-#             'name': extract_with_css('h3.author-title::text'),
-#             'birthdate': extract_with_css('.author-born-date::text'),
-#             'bio': extract_with_css('.author-description::text'),
-#         }
-
 
 class DY1234Spider(scrapy.Spider):
     name = 'dy1234'
